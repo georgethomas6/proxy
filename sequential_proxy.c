123e16abb7a8f14b92e3b5d7171d7b8e4e8131d5
georgethomas6 <alexagt0@sewanee.edu>
2026-04-28 12:19:49 -0500

    broke main into several functions

--- sequential_proxy.c
+++ sequential_proxy.c
@@ -1,16 +1,13 @@
 #include "helpers/http_parsing.h"
 #include "helpers/network.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-/*
- * MODULARIZE:
- *
- *  1. read_request method (from client)
- *  2. read_response method (from server)
- *  3. forward request method (to server from proxy)
- *  4. serve response method (to client from proxy from server)
- */
-
-int handle_client(int, struct transaction *);
+int handle_client_request(int, struct transaction *);
+int send_client_request(int, struct transaction *);
+void handle_server_reponse(int, int, struct transaction *);
+int connect_to_server(struct transaction *, char *, int *);
+void handle_transaction(int conn_fd);
 int format_request(char *, char *, char **);
 void handle_SIGPIPE(int);
 
@@ -18,7 +15,7 @@ int main(int argc, char *argv[]) {
 
   if (argc != 2) {
     fprintf(stderr, "Proper usage: ./proxy <port number>");
-    exit(EXIT_SUCCESS);
+    exit(EXIT_FAILURE);
   }
 
   int listen_fd, conn_fd;
@@ -32,91 +29,52 @@ int main(int argc, char *argv[]) {
   while (1) {
     unsigned int client_len = sizeof(client_addr);
     conn_fd = accept(listen_fd, &client_addr, &client_len);
-    if (conn_fd == -1) {
-      perror("Failed to accept connection");
-      continue;
-    }
-
-    char request[MAXLINE], response[MAXLINE], uri[MAXLINE], hostname[MAXLINE],
-        pathname[MAXLINE];
-    char *to_server = NULL;
-    int port = 80;
-
-    printf("READING\n");
-    int num_bytes = socket_read_request(conn_fd, request);
-    if (num_bytes <= 0) {
-      fprintf(stderr, "Failed to read request...aborting connection\n");
-      continue;
-    }
-
-    printf("GOT %d bytes \n", num_bytes);
-
-    print_request(request, num_bytes);
-
-    // validate request and put uri in uri variable
-    if (validate_request(request, uri, num_bytes) == 0) {
-      fprintf(stderr, "Invalid request...aborting connection\n");
-      close(conn_fd);
-      continue;
-    }
-
-    // parse uri
-    if (parse_uri(uri, hostname, pathname, &port) < 0) {
-      fprintf(stderr, "Failed to parse uri...aborting connection\n");
-      close(conn_fd);
-      continue;
-    };
-
-    // format request to send to server
-    int request_len;
-    if ((request_len = format_request(hostname, pathname, &to_server)) == -1) {
-      fprintf(stderr, "Failed to format request...aborting connection\n");
-      close(conn_fd);
-      continue;
-    }
-
-    char *p;
-    asprintf(&p, "%d", port);
-
-    int server_fd;
-    if ((server_fd = gts_open_clientfd(hostname, p)) < 0) {
-      if (server_fd == -1) {
-        fprintf(stderr, "Failed to connected to %s...aborting connection\n",
-                hostname);
-      } else {
-        const char *err = gai_strerror(server_fd * -1);
-        fprintf(stderr, "Failed to connect to %s: %s...aborting connection\n",
-                hostname, err);
-      }
-      free(to_server);
-      close(conn_fd);
-      continue;
-    };
-
-    num_bytes = socket_write(server_fd, to_server, request_len);
-    printf("WROTE %d BYTES TO SERVER", num_bytes);
-
-    num_bytes = socket_read_response(server_fd, response);
-    printf("RESPONSE: %s", response);
-    print_request(response, MAXLINE);
-
-    if (num_bytes >= 0) {
-      printf("%s", response);
-      write(conn_fd, response, num_bytes);
-    } else {
-      fprintf(stderr, "SOMETHING FAILED MAN IDEK AT THIS POINT\n");
-      continue;
-    }
-
-    // send to server and await response
-    free(to_server);
-    free(p);
-    close(server_fd);
-    close(conn_fd);
+    handle_transaction(conn_fd);
   }
 
   close(listen_fd);
   return EXIT_SUCCESS;
+}
+
+/*
+ * Writes the client's request to server_fd, on error it prints a message to
+ * stderr and returns -1
+ * On success it returns 1;
+ *
+ * */
+int send_client_request(int server_fd, struct transaction *t) {
+  // format request to send to server
+  int request_len;
+  // may need to make t.to_server by reference
+  if ((request_len =
+           format_request(t->hostname, t->pathname, &(t->to_server))) == -1) {
+    perror("Failed to format request...aborting connection\n");
+    return -1;
+  }
+
+  if ((t->to_server_len = socket_write(server_fd, t->to_server, request_len)) ==
+      request_len) {
+    return 1;
+  }
+  perror("Failed to write request to server...aborting connection\n");
+  return -1;
+}
+
+/*
+ * This function handles the server's response to the client i.e. it reads the
+ * server's response and sends it to the client if successful, otherwise it
+ * prints a message to stderr and returns -1
+ * Returns 0 on success
+ * */
+void handle_server_reponse(int server_fd, int conn_fd, struct transaction *t) {
+
+  int num_bytes;
+  num_bytes = socket_read_response(server_fd, t->response);
+
+  if (num_bytes >= 0)
+    num_bytes = write(conn_fd, t->response, num_bytes);
+  else
+    perror("Failed to read server response\n");
 }
 
 /*
@@ -125,40 +83,104 @@ int main(int argc, char *argv[]) {
  * Returns 0 on success, -1 on error
  * */
 int format_request(char *hostname, char *pathname, char **request) {
-  int ok = asprintf(request,
-                    "GET %s HTTP/1.0\r\nHost: %s\r\nConnection: "
-                    "close\r\n\r\n",
-                    pathname, hostname);
-  return ok;
+  return asprintf(request,
+                  "GET %s HTTP/1.0\r\nHost: %s\r\nConnection: "
+                  "close\r\n\r\n",
+                  pathname, hostname);
 }
 
-int handle_client(int conn_fd, struct transaction *t) {
+/*
+ * This function attempts to connect to the server whose hostname is t->hostname
+ * at port p. The hostname must be populated before function call.
+ * If an error is encountered, it prints a message to stderr and returns -1.
+ * Returns 0 on success
+ */
+int connect_to_server(struct transaction *t, char *p, int *server_fd) {
+  if ((*server_fd = gts_open_clientfd(t->hostname, p)) < 0) {
+    if (*server_fd == -1) {
+      fprintf(stderr, "Failed to connected to %s...aborting connection\n",
+              t->hostname);
+    } else {
+      const char *err = gai_strerror(*server_fd * -1);
+      fprintf(stderr, "Failed to connect to %s: %s...aborting connection\n",
+              t->hostname, err);
+    }
+    return -1;
+  }
+  return 1;
+}
+
+/*
+ * This function reads the request from socket at conn_fd, validates it, and
+ * parses it into fields in struct transaction t. Connection at conn_fd must be
+ * established beforehand.
+ * If it encounters an error, then it prints a message to stderr and returns -1
+ * On success it returns 0;
+ */
+int handle_client_request(int conn_fd, struct transaction *t) {
 
   int num_bytes = socket_read_request(conn_fd, t->request);
   if (num_bytes <= 0) {
-    fprintf(stderr, "Failed to read request...aborting connection\n");
-    close(conn_fd);
+    perror("Failed to read request...aborting connection\n");
     return -1;
   }
 
   // validate request and put uri in uri variable
   if (validate_request(t->request, t->uri, num_bytes) == 0) {
     fprintf(stderr, "Invalid request...aborting connection\n");
-    close(conn_fd);
     return -1;
   }
 
   // parse uri
-  if (parse_uri(t->uri, t->hostname, t->pathname, t->port) < 0) {
+  if (parse_uri(t->uri, t->hostname, t->pathname, &t->port) < 0) {
     fprintf(stderr, "Failed to parse uri...aborting connection\n");
-    close(conn_fd);
     return -1;
   };
-  return 0;
+  return 1;
 }
 
 void handle_SIGPIPE(int signum) {
   printf("GOT SIGPIPE SHUTTING THIS HO DOWN\n");
 
   exit(1);
+}
+
+void handle_transaction(int conn_fd) {
+
+  if (conn_fd == -1) {
+    perror("Failed to accept connection");
+    return;
+  }
+
+  struct transaction t;
+
+  if (handle_client_request(conn_fd, &t) < 0) {
+    close(conn_fd);
+    return;
+  };
+
+  char *p;
+  asprintf(&p, "%d", t.port);
+
+  int server_fd;
+  if (connect_to_server(&t, p, &server_fd) < 0) {
+    free(p);
+    close(conn_fd);
+    return;
+  };
+
+  if (send_client_request(server_fd, &t) < 0) {
+    free(p);
+    free(t.to_server);
+    close(server_fd);
+    close(conn_fd);
+    return;
+  }
+
+  handle_server_reponse(server_fd, conn_fd, &t);
+
+  free(p);
+  free(t.to_server);
+  close(server_fd);
+  close(conn_fd);
 }
